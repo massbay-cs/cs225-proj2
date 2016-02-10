f75c9f4700962a6071b84be2f080772630a50eda
Alex <pchel66694@gmail.com>
2016-02-10 12:19:41 -0500

    Start of the game class

---
+++ src/main/java/Game.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Puzzle");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        for (int i = 0)
+        grid.add()
+
+        Button StartOver = new Button("Start Over");
+        Button Hint = new Button("Hint");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BASELINE_CENTER);
+        hbBtn.getChildren().add(Hint);
+        hbBtn.getChildren().add(StartOver);
+        grid.add(hbBtn,1,10);
+
+        primaryStage.setScene(new Scene(grid, 300, 300));
+        primaryStage.show();
+    }
+}
